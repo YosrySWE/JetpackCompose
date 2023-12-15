8aa8379e254b4ea32b388f9be0da6243a7c779be
yousry <yosry_softwaredeveloper@gmail.com>
2023-12-15 16:13:10 +0300

    add rowcolumn Module

--- app/02-rowcolumn/src/main/java/com/yosry/rowcolumn/MainActivity.kt
+++ app/02-rowcolumn/src/main/java/com/yosry/rowcolumn/MainActivity.kt
@@ -3,12 +3,21 @@ package com.yosry.rowcolumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.yosry.rowcolumn.ui.theme.ComposeTheme
 
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Android studio")
                 }
             }
         }
@@ -41,6 +50,52 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeTheme {
-        Greeting("Android")
+        Greeting("studio")
+    }
+}
+@Preview
+@Composable
+fun SimpleRow(){
+    Row {
+        Text(text = "Row Text 1",Modifier.background(Color.Red))
+        Text(text = "Row Text 2",Modifier.background(Color.White))
+        Text(text = "Row Text 3",Modifier.background(Color.Green))
+    }
+}
+
+@Preview
+@Composable
+fun SimpleColumn(){
+    Column {
+        Text(text = "Column Text 1",
+            Modifier.background(Color.Red))
+        Text(text = "Column Text 2", Modifier.background(Color.White))
+        Text(text = "Column Text 3", Modifier.background(Color.Green))
+    }
+}
+
+@Preview
+@Composable
+fun RowArrangement(){
+    Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement  =  Arrangement.SpaceEvenly) {
+        Text(text = " Text 1")
+        Text(text = " Text 2")
+        Text(text = " Text 3")
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true, device = Devices.NEXUS_5, fontScale = 5f )
+@Composable
+fun ColumnArrangement(){
+    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Red),
+
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment  =  Alignment.CenterHorizontally
+    ) {
+        Text(text = "Text 1",Modifier.background(Color.Magenta))
+        Text(text = "Text 2",Modifier.background(Color.White))
+        Text(text = "Text 3",Modifier.background(Color.Green))
     }
 }
